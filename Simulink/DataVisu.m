4a39a6194159b2a8a48a6ed8391967c5ffee3077
rdelpoux <r.delpoux@gmail.com>
2021-11-16 22:52:14 +0100

    Modifictaion potentiometre + time step

--- Simulink/DataVisu.m
+++ Simulink/DataVisu.m
@@ -8,9 +8,9 @@ set(0,'CurrentFigure',50);  % select fig
 if ~isempty(A)
     clf 
     t = ([0:(length(A)-1)]  + tUart(1))*Tserial; % Do not rely on UART data arrival timestamp. Recreate time vector
-    subplot(3,1,1); plot(t,A(:,2:3)); axis tight; ylabel('v_d_q (in V)'); ylim([0 12]);    
-    subplot(3,1,2); plot(t,A(:,4:5)); axis tight; ylabel('i_d_q (in A)'); ylim([0 4]);
-    subplot(3,1,3); plot(t,A(:,6)); axis tight; ylabel('om (in rad/s)'); ylim([0 300]);
+    subplot(3,1,1); plot(t,A(:,2:3)); axis tight; ylabel('v_d_q (in V)'); ylim([-12 12]);    
+    subplot(3,1,2); plot(t,A(:,4:5)); axis tight; ylabel('i_d_q (in A)'); ylim([-4 4]);
+    subplot(3,1,3); plot(t,A(:,6:7)); axis tight; ylabel('om, omr (in rad/s)'); ylim([-380 380]);
     xlabel('time (in s)')
     dim=[0 .9 .1 .1] ;
     if(A(end,1)==0)
